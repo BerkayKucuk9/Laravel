e332b85b6592864ac3afcbfe15b1b8011da46469
BerkayKucuk9 <67931933+BerkayKucuk9@users.noreply.github.com>
2021-06-05 01:27:02 -0700

    Update create.blade.php

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -3,6 +3,17 @@
 @section('content')
 
 <h1>Create a Student</h1>
+<h1>Add New Post</h1>
 
+<form method="POST" action="{{ route('post.addStudent') }}">
+@csrf
+<label for="title">Student name:</label>
+<input type="text" name="title" placeholder="Enter post title..."> <br>
+<label for="title">Student surname:</label>
+<input type="text" name="title" placeholder="Enter post title..."> <br>
+<label for="post">Student Description:</label>
+<textarea name="post" rows="3"></textarea>
+<input type="submit" value="submit">
+</form>
 
 @endsection
